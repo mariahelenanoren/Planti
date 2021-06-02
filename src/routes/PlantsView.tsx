7a718e96a2b5c301b0d2ef6b97de1276c064536b
mariahelenanoren <marialinder97@hotmail.com>
2021-06-02 14:53:52 +0200

    added progress bar

--- src/routes/PlantsView.tsx
+++ src/routes/PlantsView.tsx
@@ -1,81 +1,97 @@
-import { Grid } from "@material-ui/core";
-import { Link } from "react-router-dom";
-import { makeStyles } from "@material-ui/core/styles";
-import classNames from "classnames";
-import React, { useEffect, useState } from "react";
-import PlantCard from "../components/PlantCard";
-import { makeRequest } from "../helper";
-import { Plant } from "../interfaces";
-import { globalStyles } from "../style/globalStyles";
+import { CircularProgress, Grid } from '@material-ui/core';
+import { Link } from 'react-router-dom';
+import { makeStyles } from '@material-ui/core/styles';
+import classNames from 'classnames';
+import React, { useEffect, useState } from 'react';
+import PlantCard from '../components/PlantCard';
+import { makeRequest } from '../helper';
+import { Plant } from '../interfaces';
+import { globalStyles } from '../style/globalStyles';
 
 const useStyles = makeStyles((theme) => ({
-  plantsContainer: {
-    paddingTop: "2rem",
-    paddingBottom: "2rem",
-    "& h2": {
-      fontSize: "1.4rem",
+    plantsContainer: {
+        paddingTop: '2rem',
+        paddingBottom: '2rem',
+        '& h2': {
+            fontSize: '1.4rem',
+        },
     },
-  },
-  cardContainer: {
-    height: "100%",
-    width: "100%",
-  },
-  buttonCard: {
-    padding: "2rem",
-    minHeight: "16rem",
-    backgroundColor: theme.palette.primary.main,
-    "& p": {
-      color: "#ffff",
-      fontWeight: 700,
+    cardContainer: {
+        height: '100%',
+        width: '100%',
     },
-    "&:hover": {
-      backgroundColor: theme.palette.secondary.dark,
+    buttonCard: {
+        padding: '2rem',
+        minHeight: '16rem',
+        backgroundColor: theme.palette.primary.main,
+        '& p': {
+            color: '#ffff',
+            fontWeight: 700,
+        },
+        '&:hover': {
+            backgroundColor: theme.palette.secondary.dark,
+        },
     },
-  },
+    progressContainer: {
+        height: '100%',
+    },
 }));
 
 export default function PlantsView() {
-  const classes = useStyles();
-  const global = globalStyles();
-  const [plants, setPlants] = useState<Plant[]>();
+    const classes = useStyles();
+    const global = globalStyles();
+    const [plants, setPlants] = useState<Plant[]>();
 
-  const fetchPlants = async () => {
-    const response = await makeRequest(`/api/plants`, "GET");
-    const savedPlants = await response;
-    setPlants(savedPlants);
-  };
+    const fetchPlants = async () => {
+        const response = await makeRequest(`/api/plants`, 'GET');
+        const savedPlants = await response;
+        setPlants(savedPlants);
+    };
 
-  useEffect(() => {
-    fetchPlants();
-  }, []);
+    useEffect(() => {
+        fetchPlants();
+    }, []);
 
-  return (
-    <div className={classNames(classes.plantsContainer, global.padding)}>
-      <div className={classNames(global.maxWidth)}>
-        <h2 className={global.textCenter}>Dina Växter</h2>
-        <Grid container spacing={3}>
-          <Grid item xs={6} sm={4} md={3}>
-            <Link to="/plants/create" className={global.link}>
-              <div
-                className={classNames(
-                  classes.buttonCard,
-                  classes.cardContainer,
-                  global.flexCenter
+    return (
+        <div className={classNames(classes.plantsContainer, global.padding)}>
+            <div className={classNames(global.maxWidth)}>
+                <h2 className={global.textCenter}>Dina Växter</h2>
+                {!plants ? (
+                    <div
+                        className={classNames(
+                            global.flexCenter,
+                            classes.progressContainer
+                        )}
+                    >
+                        <CircularProgress color='primary' />
+                    </div>
+                ) : (
+                    <Grid container spacing={3}>
+                        <Grid item xs={6} sm={4} md={3}>
+                            <Link to='/plants/create' className={global.link}>
+                                <div
+                                    className={classNames(
+                                        classes.buttonCard,
+                                        classes.cardContainer,
+                                        global.flexCenter
+                                    )}
+                                >
+                                    <p className={global.textCenter}>
+                                        Lägg till ny växt
+                                    </p>
+                                </div>
+                            </Link>
+                        </Grid>
+                        {plants.map((plant) => (
+                            <Grid item xs={6} sm={4} md={3} key={plant.id}>
+                                <div className={classes.cardContainer}>
+                                    <PlantCard plant={plant} />
+                                </div>
+                            </Grid>
+                        ))}
+                    </Grid>
                 )}
-              >
-                <p className={global.textCenter}>Lägg till ny växt</p>
-              </div>
-            </Link>
-          </Grid>
-          {plants?.map((plant) => (
-            <Grid item xs={6} sm={4} md={3} key={plant.id}>
-              <div className={classes.cardContainer}>
-                <PlantCard plant={plant} />
-              </div>
-            </Grid>
-          ))}
-        </Grid>
-      </div>
-    </div>
-  );
+            </div>
+        </div>
+    );
 }
