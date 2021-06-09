3bbc0dafd41fa41e1ef6ab8de759879ab50b3750
mariahelenanoren <marialinder97@hotmail.com>
2021-06-09 10:31:04 +0200

    fixed minor styling issues

--- src/style/globalStyles.tsx
+++ src/style/globalStyles.tsx
@@ -1,38 +1,38 @@
-import { makeStyles } from "@material-ui/core/styles";
+import { makeStyles } from '@material-ui/core/styles';
 
 export const globalStyles = makeStyles((theme) => ({
-  flex: {
-    display: "flex",
-  },
-  flex1: {
-    flex: 1,
-  },
-  maxWidth: {
-    maxWidth: "100rem",
-    margin: "auto !important",
-  },
-  padding: {
-    [theme.breakpoints.down("sm")]: {
-      paddingRight: "3rem",
-      paddingLeft: "3rem",
+    flex: {
+        display: 'flex',
     },
-    [theme.breakpoints.down("xs")]: {
-      paddingRight: "2rem",
-      paddingLeft: "2rem",
+    flex1: {
+        flex: 1,
     },
-    paddingRight: "5rem",
-    paddingLeft: "5rem",
-  },
-  flexCenter: {
-    display: "flex",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-  textCenter: {
-    textAlign: "center",
-  },
-  link: {
-    color: "inherit",
-    textDecoration: "inherit",
-  },
+    maxWidth: {
+        maxWidth: '100rem',
+        margin: 'auto !important',
+    },
+    padding: {
+        [theme.breakpoints.down('sm')]: {
+            paddingRight: '2.5rem',
+            paddingLeft: '2.5rem',
+        },
+        [theme.breakpoints.down('xs')]: {
+            paddingRight: '1.5rem',
+            paddingLeft: '1.5rem',
+        },
+        paddingRight: '4.5rem',
+        paddingLeft: '4.5rem',
+    },
+    flexCenter: {
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    textCenter: {
+        textAlign: 'center',
+    },
+    link: {
+        color: 'inherit',
+        textDecoration: 'inherit',
+    },
 }));
